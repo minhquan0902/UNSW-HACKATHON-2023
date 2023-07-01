f32f9b4d655f4d71993f5b69321cf7abd7420142
Quan Nguyen <minhquan0902@gmail.com>
2023-07-01 17:27:04 +1000

    add files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
