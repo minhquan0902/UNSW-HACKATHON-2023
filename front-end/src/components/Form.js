eb001ca63def1a834a68d2c9f0612e4379d86469
Van Nam Duong <dvannam2019@gmail.com>
2023-07-01 18:00:16 +1000

    aadd Theme

--- front-end/src/components/Form.js
+++ front-end/src/components/Form.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Form = () => {
-  return <h1>Hello World</h1>;
+  return <div class="container">
+    <h2>It's now or never</h2>
+    <h1>Come on, Join us!</h1>
+
+    <form action="" id="join-us">
+      <div class="fields">
+        <span>
+          <input placeholder="Where u wanna go" type="text" />
+        </span>
+        <br />
+        <span>
+          <input placeholder="Password" type="password" />
+        </span>
+      </div>
+      <div class="submit">
+        <input class="submit" value="Submit" type="button" />
+      </div>
+    </form>
+  </div>;
 };
 
 export default Form;
