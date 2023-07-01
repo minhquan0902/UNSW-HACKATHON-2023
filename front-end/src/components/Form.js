01cc8ee56778e5a78cb76df895b529249d14e732
Quan Nguyen <minhquan0902@gmail.com>
2023-07-02 03:28:49 +1000

    updated form

--- front-end/src/components/Form.js
+++ front-end/src/components/Form.js
@@ -1,13 +1,71 @@
 import React, { useState } from "react";
 import "../style/Form.css";
+import dataCity from "./cities.json";
+import { Autocomplete, TextField } from "@mui/material";
 
 const Form = () => {
+  dataCity = [
+    "New York City, USA",
+    "London, UK",
+    "Tokyo, Japan",
+    "Paris, France",
+    "Los Angeles, USA",
+    "Rome, Italy",
+    "Beijing, China",
+    "Mumbai, India",
+    "Sydney",
+    "Cairo, Egypt",
+    "Dubai, UAE",
+    "Shanghai, China",
+    "Istanbul, Turkey",
+    "Rio de Janeiro, Brazil",
+    "Singapore",
+    "Moscow, Russia",
+    "Hong Kong, China",
+    "Barcelona, Spain",
+    "Toronto, Canada",
+    "Mexico City, Mexico",
+    "Buenos Aires, Argentina",
+    "San Francisco, USA",
+    "Berlin, Germany",
+    "Madrid, Spain",
+    "Bangkok, Thailand",
+    "Chicago, USA",
+    "Vienna, Austria",
+    "Melbourne, Australia",
+    "Cape Town, South Africa",
+    "Seoul, South Korea",
+    "Athens, Greece",
+    "Lisbon, Portugal",
+    "Amsterdam, Netherlands",
+    "Jerusalem, Israel",
+    "Vancouver, Canada",
+    "Jakarta, Indonesia",
+    "Kuala Lumpur, Malaysia",
+
+    "São Paulo, Brazil",
+    "Miami, USA",
+    "Las Vegas, USA",
+    "Milan, Italy",
+    "Delhi, India",
+    "Montreal",
+    "Edinburgh",
+    "Dublin",
+    "Honolulu",
+    "Marrakech",
+    "Florence",
+    "Prague",
+  ];
   const [location, setLocation] = useState("");
   const [date, setDate] = useState("");
   const [people, setPeople] = useState("");
 
-  const handleLocationChange = (event) => {
-    setLocation(event.target.value);
+  const [loading, setLoading] = useState(false);
+  const [mapData, setMapData] = useState(null);
+
+  const handleAutoCompleteChange = (event, value) => {
+    setLocation(value);
+    console.log(value);
   };
 
   const handleDate = (event) => {
@@ -24,10 +82,13 @@ const Form = () => {
     // Create the payload for the POST request
     const payload = {
       location: location,
-      day: date,
-      people: people,
+      dayNum: date,
+      type: people,
     };
 
+    console.log("payload", payload);
+
+    setLoading(true);
     // Make the API POST request
     fetch(
       "https://v5ke4o8bb8.execute-api.ap-southeast-2.amazonaws.com/dev/travel",
@@ -43,47 +104,92 @@ const Form = () => {
       .then((data) => {
         // Handle the response from the server
         console.log(data);
+        setLoading(false);
       })
       .catch((error) => {
         // Handle any errors that occurred during the request
         console.log(error);
+        setLoading(false);
       });
   };
 
   return (
     <div className="container">
-      <h2>It's now or never</h2>
-      <h1>Come on, Join us!</h1>
+      <h2>AnhEmTravelling</h2>
+      <h1>Where d'you wanna go?</h1>
 
       <form action="" onSubmit={handleSubmit} id="join-us">
-        <div className="fields">
-          <span>
-            <input
-              placeholder="Hi, where u wanna go"
-              type="text"
-              value={location}
-              onChange={handleLocationChange}
+        <Autocomplete
+          options={dataCity}
+          getOptionLabel={(option) => option}
+          value={location}
+          style={{
+            color: "black",
+
+            borderRadius: 2,
+          }}
+          sx={{ width: "220px", marginRight: 3 }}
+          hiddenLabel="true"
+          onChange={handleAutoCompleteChange}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: "50px",
+
+                  legend: {
+                    marginLeft: "30px",
+                  },
+                },
+                "& .MuiAutocomplete-inputRoot": {
+                  paddingLeft: "20px !important",
+                  borderRadius: "50px",
+                },
+                "& .MuiInputLabel-outlined": {
+                  paddingLeft: "20px",
+                },
+                "& .MuiInputLabel-shrink": {
+                  marginLeft: "20px",
+                  paddingLeft: "10px",
+                  paddingRight: 0,
+                  background: "white",
+                },
+              }}
+              placeholder="Select a destination"
+              InputProps={{
+                ...params.InputProps,
+                style: {
+                  borderRadius: "1.6rem",
+                  backgroundColor: "white",
+                  height: "34px",
+                },
+              }}
             />
-          </span>
-          <br />
-          <span>
-            <input
-              placeholder="Number of days"
-              type="text"
-              value={date}
-              onChange={handleDate}
-            />
-          </span>
-          <br />
-          <span>
-            <input
-              placeholder="Number of people"
-              type="text"
-              value={people}
-              onChange={handlePeople}
-            />
-          </span>
-        </div>
+          )}
+        />
+
+        <br />
+        <br />
+        <span>
+          <input
+            style={{ color: "black" }}
+            placeholder="Number of days"
+            type="text"
+            value={date}
+            onChange={handleDate}
+          />
+        </span>
+        <br />
+        <span>
+          <input
+            placeholder="Solo, Friends, Family"
+            type="text"
+            value={people}
+            onChange={handlePeople}
+          />
+        </span>
+
         <div className="submit">
           <input className="submit" value="Submit" type="submit" />
         </div>
