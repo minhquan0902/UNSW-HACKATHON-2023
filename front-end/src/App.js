1f7dce3a1d9c2d03434ff5359a52af2b26c038f7
Quan Nguyen <minhquan0902@gmail.com>
2023-07-02 05:45:11 +1000

    add protected for mobile

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,34 @@
+import React, { useState, useEffect } from "react";
 import Form from "./components/Form";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div>
-      <Form />
+      {isMobile ? (
+        <h2
+          style={{
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          Our App is only available in widescreen... So sorry for this!˙◠˙
+        </h2>
+      ) : (
+        <Form />
+      )}
     </div>
   );
 }
